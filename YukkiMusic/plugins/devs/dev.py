f2621e137e4ba03c2c51861ac0ac4ee729f94dd6
Team Inflex <TheTeamInflex@Gmail.com>
2023-07-29 19:34:39 -0700

    Update dev.py
    Done Eval

--- YukkiMusic/plugins/devs/dev.py
+++ YukkiMusic/plugins/devs/dev.py
@@ -19,6 +19,7 @@ import traceback
 from inspect import getfullargspec
 from io import StringIO
 from time import time
+from config import OWNER_ID
 
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardButton,
@@ -44,7 +45,7 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 @app.on_message(
     filters.command("inflex")
-    & filters.user(5747402681)
+    & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
